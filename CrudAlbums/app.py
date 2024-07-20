a11d084f4cb6e0c77687dadaa89a5a6c330bb3c0
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-07-20 14:53:07 -0600

    Actividad 14 terminada

--- CrudAlbums/app.py
+++ CrudAlbums/app.py
@@ -1,103 +1,118 @@
-#hacer las importaciones necesarias para la práctica
-#para renderizar o generar la vista se necesita el render_template
-#la url_for hacer la redirección en conjunto con redirect y flass nos ayuda a procesar el mensaje
 from flask import Flask, request, render_template, url_for, redirect, flash
-#agregar una importación de lo que se necesite de la base de dato
 from flask_mysqldb import MySQL
+#se utiliza para enviar archivos desde un directorio específico en el servidor
+from flask import send_from_directory 
+#según el video este es para evitar archivos dañados
+from werkzeug.utils import secure_filename
+# segun el video proporciona una forma de usar funcionalidades dependientes del sistema operativo
+import os
+#se usa como una constante que especifica el directorio donde se guardarán los archivos subidos
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
-#declara una variable
-app=Flask(__name__)
+app = Flask(__name__)
 
-#da de alta las variables de la configuración de la bd
-app.config['MYSQL_HOST']='localhost'
-#da de alta lo del usuario con el que te conectas a la bd
-app.config['MYSQL_USER']='root'
+# Configuración de MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'bdflask'
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Est carpeta es para almacenar imágenes
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'png'} #esta es para las extensiones de las imagenes permitidas
+app.secret_key = 'mysecretkey'
 
-app.config['MYSQL_PASSWORD']=''
-#contraseña de la bd que por lo general viene vacia
-app.config['MYSQL_DB']='bdflask'
+mysql = MySQL(app)
 
-app.secret_key='mysecretkey'
-
-#crear una variable que se va a usar para la conexion a la bd
-mysql= MySQL(app)
-
-#crear una ruta para ejecutar la vista index
+# De acuerdo al video esta función que me sirve para verificar la extensión del archivo
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 def index():
     try:
-        cursor= mysql.connection.cursor();
-        cursor.execute('select * from albums')
-        consultaA= cursor.fetchall()
-        return render_template('index.html', albums= consultaA)
-        #redireccion y resultado de la consulta
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM albums')
+        consultaA = cursor.fetchall()
+        return render_template('index.html', albums=consultaA)
     except Exception as e:
-        print('e')
+        print(e)
 
-    
-
-#crear una función para mandar los datos al momento de pulsar el boton.
 @app.route('/guardarAlbum', methods=['POST'])
 def guardarAlbum():
     if request.method == 'POST':
-        #tomamos los datso que vienen por POST
-        Ftitulo= request.form['txtTitulo']
-        Fartista=request.form['txtArtista']
-        Fanio=request.form['txtAnio']
+        Ftitulo = request.form['txtTitulo']
+        Fartista = request.form['txtArtista']
+        Fanio = request.form['txtAnio']
+        # nos ayuda a verificar las imagenes que si sean del formato
+        file = request.files['file']
+        if file and allowed_file(file.filename): #Verifica si el nombre del archivo tiene una extensión permitida
+            filename = secure_filename(file.filename)# para asegurar que el nombre del archivo sea seguro para almacenar en el sistema de archivos.
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))# guarda el archivo en la ruta especificada.
+            portada_url = url_for('uploaded_file', filename=filename) #crea una URL para la ruta que sirve archivos subidos. 
+        else:
+        #en caso de que no sea se manda un mensaje de alerta
+            flash('Tipo de archivo no permitido. Solo se permiten archivos .jpg y .png.')
+            return redirect(url_for('index'))
         
-        #Enviamos a la BD
-        cursor= mysql.connection.cursor()
-        #mandamos un insert del formulario hacia nuestra base de datos
-        cursor.execute('insert into albums(titulo,artista,anio) values(%s,%s,%s)', (Ftitulo,Fartista,Fanio))
-        #mandamos el commit
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO albums (titulo, artista, anio, portada) VALUES (%s, %s, %s, %s)', (Ftitulo, Fartista, Fanio, portada_url))
         mysql.connection.commit()
         
-        flash('Album Guardado corretamente')
-        #hacemos que una vez guardado el dato que redireccione al index
+        flash('Álbum guardado correctamente')
         return redirect(url_for('index'))
 
 @app.route('/editar/<id>')
 def editar(id):
-    cur= mysql.connection.cursor()
-    cur.execute ('select * from albums where id_album=%s',[id])
-    albumE= cur.fetchone()
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM albums WHERE id_album = %s', [id])
+    albumE = cur.fetchone()
     return render_template('editar.html', album=albumE)
 
 @app.route('/ActualizarAlbum/<id>', methods=['POST'])
 def ActualizarAlbum(id):
     if request.method == 'POST':
-        #tomamos los datso que vienen por POST
-        Ftitulo= request.form['txtTitulo']
-        Fartista=request.form['txtArtista']
-        Fanio=request.form['txtAnio']
-        #Enviamos a la BD
-        cursor= mysql.connection.cursor()
-        #mandamos un insert del formulario hacia nuestra base de datos
-        cursor.execute('update albums set titulo= %s,artista= %s,anio= %s where id_album= %s', (Ftitulo,Fartista,Fanio,id) )
-        #mandamos el commit
+        Ftitulo = request.form['txtTitulo']
+        Fartista = request.form['txtArtista']
+        Fanio = request.form['txtAnio']
+        
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],  filename))
+            portada_url = url_for('uploaded_file', filename=filename)
+        else:
+            # Mantener la portada anterior si no se proporciona un nuevo archivo
+            cur = mysql.connection.cursor()
+            cur.execute('SELECT portada FROM albums WHERE id_album = %s', [id])
+            portada_url = cur.fetchone()[0]
+        
+        cursor = mysql.connection.cursor()
+        cursor.execute('UPDATE albums SET titulo = %s, artista = %s, anio = %s, portada = %s WHERE id_album = %s', (Ftitulo, Fartista, Fanio, portada_url, id))
         mysql.connection.commit()
         
-        flash('Album Editado corretamente')
-        #hacemos que una vez guardado el dato que redireccione al index
+        flash('Álbum editado correctamente')
         return redirect(url_for('index'))
 
 @app.route('/EliminarAlbum/<id>')
 def EliminarAlbum(id):
     cursor = mysql.connection.cursor()
-    cursor.execute('delete from albums WHERE id_album = %s', [id])
+    cursor.execute('DELETE FROM albums WHERE id_album = %s', [id])
     mysql.connection.commit()
         
     flash('Álbum eliminado correctamente')
     return redirect(url_for('index'))
 
+#definimos una nueva ruta que nos permite a los archivos ya subidos 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],  filename)
 
-
-#Manejo de excepciones para rutas 
+# Manejo de excepciones para rutas
 @app.errorhandler(404)
 def paginano(e):
     return 'Revisa tu sintaxis: No encontré nada'
-#ejecutar el proyecto
+
+# Ejecutar el proyecto
 if __name__ == '__main__':
-    app.run(port=3000, debug=True) 
-    #debug nos ayuda principalmente a las refrescar en caso de que haya algún cambio
+    app.run(port=3000, debug=True)
